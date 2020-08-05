570a14a0abd24bafea3bcc315d326e8deda79578
zbb <binbin.zhang@pantum.local>
2020-08-05 13:40:59 +0800

    change theme

--- js/lovestory.js
+++ js/lovestory.js
@@ -1,10 +1,28 @@
 let lovestory = [
-    '今夜月色真美',
-    '风也很温柔',
+    '晓看天色暮看云',
+    '我爱谢莹',
+];
+
+let loveyou = [
+    '行也思君，坐也思君',
+    '行也思君，坐也思君',
+    '行也思君，坐也思君',
+    '行也思君，坐也思君',
+    '行也思君，坐也思君',
+    '行也思君，坐也思君',
+    '行也思君，坐也思君',
+    '行也思君，坐也思君',
+    '行也思君，坐也思君',
+    '我爱谢莹',
 ];
 
 function randomLovestory(n) {
-    document.getElementById('sentence').innerHTML = lovestory[n];
+    if (n == 0) {
+        document.getElementById('sentence').innerHTML = lovestory[n];
+    }
+    else {
+        document.getElementById('sentence').innerHTML = loveyou[Math.floor(Math.random() * loveyou.length)];
+    }
 }
 
 randomLovestory(0);
