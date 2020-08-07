7d3cc7ca3012fbe8b0e981416712e403e4af58cc
benalex <ben_zzz@163.com>
2020-08-07 21:21:33 +0800

    update lovestory.js

--- js/lovestory.js
+++ js/lovestory.js
@@ -1,28 +1,14 @@
 let lovestory = [
-    '晓看天色暮看云',
-    '我爱谢莹',
-];
-
-let loveyou = [
-    '行也思君，坐也思君',
-    '行也思君，坐也思君',
-    '行也思君，坐也思君',
-    '行也思君，坐也思君',
-    '行也思君，坐也思君',
-    '行也思君，坐也思君',
-    '行也思君，坐也思君',
-    '行也思君，坐也思君',
-    '行也思君，坐也思君',
+    '好好生活，慢慢爱你。不早不晚，刚好是你。',
     '我爱谢莹',
 ];
 
 function randomLovestory(n) {
-    if (n == 0) {
         document.getElementById('sentence').innerHTML = lovestory[n];
-    }
-    else {
-        document.getElementById('sentence').innerHTML = loveyou[Math.floor(Math.random() * loveyou.length)];
-    }
+}
+
+function loadLovestory(n) {
+    document.getElementById('weekday').innerHTML = lovestory[n];
 }
 
 randomLovestory(0);
